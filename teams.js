ec2bd7abc129e2c06009e1b1f88af8fd203a3fb3
Comet06 <93844341+Comet06@users.noreply.github.com>
2025-09-18 13:52:56 -0400

    Update teams.js

--- teams.js
+++ teams.js
@@ -1,7 +1,7 @@
 import { Legacyplayers, Legacyteams, players, teams } from "./player_data.js";
 
 window.addEventListener('load', function() {
-  if (window.location.pathname === '/players_stats.html') {
+  if (window.location.pathname === '/RLCS-2026-Fantasy-Website/players_stats.html') {
     console.log('The specific page has loaded!');
     deployNew()
     
@@ -71,4 +71,5 @@ function deployNew() {
   }
 
 }
+
 
