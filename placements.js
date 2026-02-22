5fadacadf86d4e39a616188dc970b57dc058145f
Comet06 <93844341+Comet06@users.noreply.github.com>
2026-02-22 16:52:45 -0500

    Add files via upload

--- placements.js
+++ placements.js
@@ -57,7 +57,7 @@ export let Regional6Placements = {
 }
 export let Placements = {
     'Kickoff' : ['Karmine Corp', 'Shopify Rebellion', 'Virtus pro', 'Spacestation Gaming', 'Ninjas in Pyjamas', 'Team Vitality', 'Novo Esports', 'GenG Mobil1 Racing'],
-    'Major1' : ['TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'Geekay Esports', 'Ninjas in Pyjamas', 'PWR', 'Virtus pro', 'Twisted Minds', 'MIBR', 'Five Fears', 'Furia Esports', 'Spacestation Gaming', 'TSM'],
+    'Major1' : ['Team Vitality', 'Gentle Mates Alpine', 'NRG', 'Team Falcons', 'Karmine Corp', 'Shopify Rebellion', 'Geekay Esports', 'Ninjas in Pyjamas', 'PWR', 'Virtus pro', 'Twisted Minds', 'MIBR', 'Five Fears', 'Furia Esports', 'Spacestation Gaming', 'TSM'],
     'Major2' : ['TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD'],
     'Champ' : ['TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD', 'TBD'],
 }
